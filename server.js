ee58e7de0aeb1d4818549f2bfcffd1ca85a7a4ce
60225198 <daniel_zamora27@me.com>
2021-01-24 01:15:46 +0000

    shorter req work

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+
+// //Models
+// const URL = require('./models/schema')
+
+//server init
+const app = express();
+
+//Middleware
+app.use(bodyParser.urlencoded ({ extended: false}));
+app.use(bodyParser.json());
+
+//Path
+app.get('/', (req,res) => {
+    res.send('Home')
+})
+
+//Database uri
+const db = require('./config/uri').mongoURI;
+
+//Conect DB
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then( () => console.log('MongoDB on'))
+    .catch(err => console.log(err)); 
+
+//Endpoints
+const shorter = require('./routes/api/shorter');
+app.use('/api/shorter', shorter);
+
+
+//Port
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Server runing on port ${port}`));
